5f0acc58f19aaabb44bcd7029489a89520170445
Myles Smith <hello@itsmyl.es>
2024-02-19 10:31:51 +0000

    make user validatable

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Traits\Validatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Validation\Rule;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, Validatable;
 
     /**
      * The attributes that are mass assignable.
@@ -42,4 +45,15 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function rules()
+    {
+        return [
+            'name'=>'string|max:255|required',
+            'email'=>[
+                'required',
+                Rule::unique('users','email')->ignore($this)
+            ]
+        ];
+    }
 }
