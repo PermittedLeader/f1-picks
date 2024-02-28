e0eb693630786089591c3e6b0203960508264cc0
Myles Smith <hello@itsmyl.es>
2024-02-28 10:03:16 +0000

    Update footer

---
+++ resources/views/vendor/tiffey/components/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="flex flex-row justify-between">
+    <div>{{ config('app.name') }}</div> 
+    <div>
+        <a href="https://github.com/PermittedLeader/f1-picks/issues/new">Report a bug</a>
+    </div>
+</div>
