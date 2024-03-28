94cf9ed9155f42a5abb8f171647b2cae3cc360bf
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-03-28 17:40:38 +0000

    allow making members admins

--- resources/views/league/edit.blade.php
+++ resources/views/league/edit.blade.php
@@ -4,4 +4,12 @@
         <x-slot:header>Seasons</x-slot:header>
         <livewire:tables.league.seasons-table :league="$league" />
     </x-tiffey::card>
+    <x-tiffey::card>
+        <x-slot:header>Members</x-slot:header>
+        <livewire:tables.league.members-admin-table :league="$league" lazy relationshipName="members" :model="$league"/>
+    </x-tiffey::card>
+    <x-tiffey::card>
+        <x-slot:header>Admins</x-slot:header>
+        <livewire:tables.league.admins-table :league="$league" lazy />
+    </x-tiffey::card>
 </x-tiffey::layouts.main-layout>
