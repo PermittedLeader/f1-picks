9b12ac6243f2b284da0bf7ff865a2237d95784d3
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-07-25 10:01:02 +0100

    adding not picked component to league page

--- app/Livewire/Tables/Pick/EventNotPickedTable.php
+++ app/Livewire/Tables/Pick/EventNotPickedTable.php
@@ -14,7 +14,7 @@ use Permittedleader\TablesForLaravel\Http\Livewire\Table;
 use Permittedleader\TablesForLaravel\View\Components\Columns\Column;
 use Permittedleader\TablesForLaravel\View\Components\Columns\BelongsTo;
 
-class EventTable extends Table
+class EventNotPickedTable extends Table
 {
     public bool $isExportable = false;
 
@@ -32,15 +32,22 @@ class EventTable extends Table
 
     public function query(): Builder
     {
-        return Pick::query()->where('league_id',$this->league->id)->where('event_id',$this->event_id)->where('season_id',$this->season_id);
+        return User::whereHas("leagues", function ($query) {
+                $query->where("id", $this->league->id);
+            })
+            ->whereNotIn(
+                "id",
+                Pick::where("league_id", $this->league->id)
+                    ->where("event_id", $this->event_id)
+                    ->where("season_id", $this->season_id)
+                    ->select("user_id")
+            );
     }
 
     public function columns(): array
     {
         return [
-            BelongsTo::make('user')->model(User::class)->sortable()->filterable(),
-            BelongsTo::make('season')->model(Season::class)->sortable()->filterable(),
-            BelongsTo::make('pickable','Pick')->model(Pickable::class)->sortable()->filterable(),
+            Column::make('name')
         ];
     }
 
