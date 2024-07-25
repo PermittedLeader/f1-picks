b407610467d3be3b6b277d27165e9781294c1f29
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-07-25 20:17:47 +0100

    add order to pickables

--- app/Livewire/Tables/Season/PickableTable.php
+++ app/Livewire/Tables/Season/PickableTable.php
@@ -25,7 +25,7 @@ class PickableTable extends Table
 
     public function query(): Builder
     {
-        return $this->season->pickables();
+        return $this->season->pickables()->withPivot(['order']);
     }
 
     public function columns(): array
@@ -33,6 +33,7 @@ class PickableTable extends Table
         return [
             Column::make('name')->sortable(),
             Column::make('team')->sortable(),
+            Column::make('*','Order')->formatDisplay(fn($value)=>$value->pivot->order)->sortable(),
         ];
     }
 
@@ -46,7 +47,6 @@ class PickableTable extends Table
     public function tableActions(): array
     {
         return [
-            Action::makeClick('$wire.pickableListModal = true', 'Attach')->showLabel()->icon('fa-solid fa-link')
         ];
     }
 
