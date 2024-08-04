08c7ec6f317fb334025773cb3371673e20627ea1
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-08-04 20:38:18 +0100

    show top 5 pickables remaining

--- app/Livewire/Tables/League/MembersTable.php
+++ app/Livewire/Tables/League/MembersTable.php
@@ -6,6 +6,8 @@ use App\Models\League;
 use App\Traits\FlashMessages;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Contracts\Database\Eloquent\Builder;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\Reactive;
 use Permittedleader\TablesForLaravel\Http\Livewire\Table;
 use Permittedleader\TablesForLaravel\View\Components\Columns\Column;
 
@@ -19,9 +21,14 @@ class MembersTable extends Table
 
     public int $perPage = 25;
 
+    public int $topNRemaining = 5;
+
     public string $sortBy = 'rank';
 
     public League $league;
+
+    #[Reactive]
+    public $season_id;
 
     public string $exportName = 'users-export';
 
@@ -30,6 +37,30 @@ class MembersTable extends Table
         return $this->league->members()
         ->join("picks", "users.id", "=", "picks.user_id")
         ->where("picks.league_id", $this->league->id)
+        ->when($this->season_id,function($query){
+            $query->where('picks.season_id',$this->season_id);
+            $query->selectRaw("(
+            SELECT
+                SUBSTRING_INDEX(GROUP_CONCAT(short_name SEPARATOR ', ') ,', ','".$this->topNRemaining."')
+            FROM
+                pickables p
+            JOIN pickable_season ps on
+                p.id = ps.pickable_id
+            WHERE
+                ps.season_id = '".$this->season_id."'
+                AND id NOT IN (
+                SELECT
+                    pickable_id
+                from
+                    picks p2
+                where
+                    league_id = '".$this->league->id."'
+                    and season_id = '".$this->season_id."'
+                    and user_id = users.id)
+            ORDER BY
+                ps.`order` ASC
+                ) AS 'remainingTopPicks'");
+        })
         ->selectRaw("sum(`picks`.`score`) AS 'score'")
         ->selectRaw(
             DB::raw("DENSE_RANK() OVER (ORDER BY sum(`picks`.`score`) DESC) as \"rank\"")
@@ -40,11 +71,15 @@ class MembersTable extends Table
 
     public function columns(): array
     {
-        return [
+        $columns =  [
             Column::make('rank')->sortable(),
             Column::make('name')->sortable(),
             Column::make('score', 'Score')->sortable()
         ];
+        if($this->season_id){
+            $columns[] = Column::make('remainingTopPicks','Best '.$this->topNRemaining.' picks remaining');
+        }
+        return $columns;
     }
 
     public function actions(): array
