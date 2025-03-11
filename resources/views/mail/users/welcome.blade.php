3e574b280efbdc86dc40fee028e0c94073230d3b
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2025-03-11 11:03:22 +0000

    amend welcome email

--- resources/views/mail/users/welcome.blade.php
+++ resources/views/mail/users/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-mail::message>
 # Welcome to {{ config('app.name') }}
 
-You can join leagues and pick for events. We've signed you up to a League automatically. Hope you enjoy it! Now go ahead and dive right in
+You can join leagues and pick for events. You can join public leagues from the home page, or with a private league code and password. Hope you enjoy it! Now go ahead and dive right in
 
 <x-mail::button :url="route('welcome')">
 Your leagues
